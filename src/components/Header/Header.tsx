acc5873d11910c42e5c14dfe1e303687e99cc167
Dhruv <samantdhruv@gmail.com>
2022-06-15 23:19:37 +0530

    fix : header and responsiveness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,21 +6,26 @@ export const Header = () => {
 
   return (
     <nav>
-      <i className="theme fas fa-user"></i>
-      <a target={"_blank"} href="https://twitter.com/TheBestDhruv">
-        <i className="theme fab fa-twitter"></i>
-      </a>
-      <a
-        target={"_blank"}
-        href="https://github.com/b0llu/StormIZ/tree/developer"
-      >
-        <i className="theme fab fa-github"></i>
-      </a>
-      {theme === "light" ? (
-        <i onClick={toggleTheme} className="theme fas fa-moon"></i>
-      ) : (
-        <i onClick={toggleTheme} className="theme fas fa-sun"></i>
-      )}
+      <div className="brand-name">
+        <i className="fas fa-bolt"></i> StormIZ
+      </div>
+      <div>
+        <i className="theme fas fa-user"></i>
+        <a target={"_blank"} href="https://twitter.com/TheBestDhruv">
+          <i className="theme fab fa-twitter"></i>
+        </a>
+        <a
+          target={"_blank"}
+          href="https://github.com/b0llu/StormIZ/tree/developer"
+        >
+          <i className="theme fab fa-github"></i>
+        </a>
+        {theme === "light" ? (
+          <i onClick={toggleTheme} className="theme fas fa-moon"></i>
+        ) : (
+          <i onClick={toggleTheme} className="theme fas fa-sun"></i>
+        )}
+      </div>
     </nav>
   );
 };
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -9,10 +9,7 @@ export const LandingPage = () => {
 
   return (
     <section className="landing-page-container">
-      <h1 className="title">
-        <i className="fas fa-bolt"></i> StormIZ
-      </h1>
-      <p className="subtitle">Let's see how great of a Gamer you are!</p>
+      <p className="title">Let's see how great of a Gamer you are!</p>
       <h2 className="category-subtitle">Select a Category</h2>
       <div className="category-selector">
         {categories?.map((category) => {
