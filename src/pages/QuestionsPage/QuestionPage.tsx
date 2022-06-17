4da53f84990c5ab7f1fa80421760ca77fb4d39c6
Dhruv <samantdhruv@gmail.com>
2022-06-17 11:22:34 +0530

    style : question page styling

--- src/pages/QuestionsPage/QuestionPage.tsx
+++ src/pages/QuestionsPage/QuestionPage.tsx
@@ -1,5 +1,39 @@
+import "./QuestionPage.css";
+
 export const QuestionPage = () => {
-    return (
-        <div>hi</div>
-    )
-}
+  return (
+    <section
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div className="question-wrapper">
+        <h2 className="subtitle">I don't play the odds, I play the man</h2>
+        <div className="marking">
+          <h2 className="subtitle">
+            Question: <span className="bold">1/5</span>
+          </h2>
+          <h2 className="subtitle">
+            Score: <span className="bold">0</span>
+          </h2>
+        </div>
+        <h2 className="question">
+          Harvey Specter is considered one of New York's most brilliant
+          corporate litigation lawyers, but what's his unusual middle name ?
+        </h2>
+        <div className="answers-container">
+          <p className="answers card-shadow">Michael</p>
+          <p className="answers card-shadow">Reginald</p>
+          <p className="answers card-shadow">Brandon</p>
+        </div>
+        <a href="">
+          <span className="take-quiz">
+            See Result <i className="fas fa-chevron-right"></i>
+          </span>
+        </a>
+      </div>
+    </section>
+  );
+};
