8721cbf1f2015c39c090b5dc6333705f1578f2aa
Dhruv <samantdhruv@gmail.com>
2022-06-18 12:32:00 +0530

    add : result page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { LandingPage } from "./LandingPage/LandingPage";
 export { CategoryPage } from "./CategoryPage/CategoryPage";
 export { QuestionPage } from "./QuestionsPage/QuestionPage";
+export { ResultPage } from "./ResultPage/ResultPage";
